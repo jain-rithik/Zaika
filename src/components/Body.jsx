1756cb7424ffe1fe23b266a21b8dd32e19e50a78
Rithik <jainrithik2000@gmail.com>
2024-03-17 13:18:31 +0530

    chnaged zaika server deployed linked

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -23,7 +23,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      `https://zaika-server.onrender.com/api/restaurants?lat=${location.latitude}&lng=${location.longitude}`
+      `https://zaika.cyclic.app/api/restaurants?lat=${location.latitude}&lng=${location.longitude}`
     );
     // "https://instafood.onrender.com/api/restaurants?lat=19.0918606&lng=72.8825928"
     // https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D${location.latitude}%26lng%3D${location.longitude}
@@ -81,7 +81,7 @@ const Body = () => {
 
   const fetchMobileData = async () => {
     const mData = await fetch(
-      `https://zaika-server.onrender.com/mapi/restaurants?lat=${location.latitude}&lng=${location.longitude}`
+      `https://zaika.cyclic.app/mapi/restaurants?lat=${location.latitude}&lng=${location.longitude}`
     );
     // https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Fhomepage%2FgetCards%3Flat%3D${location.latitude}%26lng%3D${location.longitude}
     const json = await mData.json();
